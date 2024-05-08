2772ea1e4b35c6a8526a046483f431748e4e067b
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2024-05-08 19:01:54 +0530

    backend and frontend connected

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    Hello World
+    <h1>{{name}}</h1>
+    <h2>Projects</h2>
+    <div v-if="pending1">Loading...</div>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h2>{{project.name}}</h2>
+        <p>{{project.description}}</p>
+      </li>
+    </ul>
+  </div>
+
+  <div>
+    <h2>Blogs</h2>
+    <div v-if="pending2">Loading...</div>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.title">
+        <h2>{{blog.title}}</h2>
+        <p>{{blog.content}}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const name = "Nipun" ;
+const {data: projects, pending1, error1} = useFetch('http://localhost:5000/Projects');
+const {data: blogs, pending2, error2 } = useFetch('http://localhost:5000/Blogs');
+</script>
+
+<style>
+</style>
