e16cc3c15912fedba8e408bdce9791a1bf757675
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2024-05-29 00:34:16 +0530

    Created functions to delete projects and blogs.But have to refresh

--- app.vue
+++ app.vue
@@ -27,7 +27,7 @@
     </div>
   </div>
   
-  <div class="continer">
+  <div class="continer mx-5">
     <div class="row">
       <div class="col">
 
@@ -41,7 +41,7 @@
             <label for="projectDescription" class="form-label">Project Description</label>
             <textarea class="form-control" id="projectDescription" v-model="newProject.description"></textarea>
           </div>
-          <button type="submit" class="btn btn-primary">Create Project</button>
+          <button type="submit" class="btn btn-primary mb-3">Create Project</button>
         </form>
       </div>
 
@@ -57,7 +57,7 @@
             <label for="blogContent" class="form-label">Blog Content</label>
             <textarea class="form-control" id="blogContent" v-model="newBlog.content"></textarea>
           </div>
-          <button type="submit" class="btn btn-primary">Create Blog</button>
+          <button type="submit" class="btn btn-primary mb-3">Create Blog</button>
         </form>
       </div>
 
@@ -65,13 +65,37 @@
 
     <div class="row">
       <div class="col">
+
+        <!--form to delete a project by id-->
+        <form @submit="deleteProject">
+          <div class="mb-3">
+            <label for="projectId" class="form-label">Project Id</label>
+            <input type="text" class="form-control" id="projectId" v-model="deleteProjectId">
+          </div>
+            <button type="submit" class="btn btn-danger">Delete Project</button>
+        </form>
       </div>
+
+      <div class="col">
+
+        <!--form to delete a blog by id-->
+        <form @submit="deleteBlog">
+          <div class="mb-3">
+            <label for="blogId" class="form-label">Blog Id</label>
+            <input type="text" class="form-control" id="blogId" v-model="deleteBlogId">
+          </div>
+            <button type="submit" class="btn btn-danger">Delete Blog</button>
+        </form>
+      </div>
+
     </div>
   </div>
   
 </template>
 
 <script setup>
+
+
 useHead({
   link: [
     { 
@@ -87,15 +111,13 @@ const name = "Nipun" ;
 const {data: projects, pending1, error1} = useFetch('http://localhost:5000/Projects');
 const {data: blogs, pending2, error2 } = useFetch('http://localhost:5000/Blogs');
 
-//catching form data on project
-
+//variable to catch form data on project
 const newProject = ref({
   name: '',
   description: ''
 });
 
 //function to create projects 
-
 const createProject = async () => {
   console.log(newProject.value);
 
@@ -126,15 +148,39 @@ const createProject = async () => {
   }
 }
 
-//catching form data on blog
+//variable to catch  project id to delete
+const deleteProjectId = ref('');
 
+//function to delete projects
+const deleteProject = async () => {
+  console.log(deleteProjectId.value);
+
+  try {
+    const response = await fetch(`http://localhost:5000/Projects/${deleteProjectId.value}`, {
+      method: 'DELETE'
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to delete project');
+    }
+
+    // Removing the project from the list of projects
+    projects.value = projects.value.filter(project => project.id !== deleteProjectId.value);
+
+    // Resetting the form
+    deleteProjectId.value = '';
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+//variable to catch form data on blog
 const newBlog = ref({
   title: '',
   content: ''
 });
 
 //function to create blogs
-
 const createBlog = async () => {
   console.log(newBlog.value);
 
@@ -164,6 +210,32 @@ const createBlog = async () => {
     console.error("Error",error);
   }
 }
+
+//variable to catch  project id to delete
+const deleteBlogId = ref('');
+
+//function to delete blogs
+const deleteBlog = async () => {
+  console.log(deleteBlogId.value);
+
+  try {
+    const response = await fetch(`http://localhost:5000/Blogs/${deleteBlogId.value}`, {
+      method: 'DELETE'
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to delete blog');
+    }
+
+    // Removing the blog from the list of blogs
+    blogs.value = blogs.value.filter(blog => blog.id !== deleteBlogId.value);
+
+    // Resetting the form
+    deleteBlogId.value = '';
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
 
 </script>
 
